72ff36ecb02b5699d1dd675e1b9d6fd5b8f81e91
Asel Turatbek Kyzy <aselturatbekk@gmail.com>
2025-05-18 23:22:00 +0300

    show/hide password, activity loader

--- src/pages/LoginScreen.js
+++ src/pages/LoginScreen.js
@@ -9,29 +9,43 @@ import {
   KeyboardAvoidingView,
   Platform,
   ScrollView,
+  ActivityIndicator
 } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
 import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const API_URL = 'https://masal-backend-on7u.onrender.com/auth';
+import { API_URL } from '@env';
+
 
 export default function LoginScreen({ navigation }) {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+  const [showPassword, setShowPassword] = useState(false);
+
 
   const handleLogin = async () => {
-    try {
-      const response = await axios.post(`${API_URL}/login`, { email, password });
-      const token = response.data.access_token;
-      const name = response.data.name;
-      await AsyncStorage.setItem('token', token);
-      await AsyncStorage.setItem('name', name);
-      navigation.replace('Home');
-    } catch (error) {
-      const msg = error?.response?.data?.message || 'Bir hata oluştu';
-      Alert.alert('Hata', msg.toString());
-    }
-  };
+  if (!email || !password) {
+    Alert.alert('Eksik Bilgi', 'Lütfen e-posta ve şifrenizi girin.');
+    return;
+  }
+
+  try {
+    setIsLoading(true); // Yüklenme başlasın
+    const response = await axios.post(`${API_URL}/auth/login`, { email, password });
+    const token = response.data.access_token;
+    const name = response.data.name;
+    await AsyncStorage.setItem('token', token);
+    await AsyncStorage.setItem('name', name);
+    navigation.replace('Home');
+  } catch (error) {
+    const msg = error?.response?.data?.message || 'Bir hata oluştu';
+    Alert.alert('Hata', msg.toString());
+  } finally {
+    setIsLoading(false); // Yüklenme bitsin
+  }
+};
 
   return (
     <KeyboardAvoidingView
@@ -54,15 +68,23 @@ export default function LoginScreen({ navigation }) {
           autoCapitalize="none"
           keyboardType="email-address"
         />
+        <View style={styles.passwordContainer}>
         <TextInput
-          style={styles.input}
+          style={styles.passwordInput}
           placeholder="Şifre"
           value={password}
           onChangeText={setPassword}
           placeholderTextColor="#aaa"
-          secureTextEntry
+          secureTextEntry={!showPassword}
         />
-
+        <TouchableOpacity onPress={() => setShowPassword(!showPassword)}>
+          <Ionicons
+            name={showPassword ? 'eye-off' : 'eye'}
+            size={22}
+            color="#A75CFF"
+          />
+        </TouchableOpacity>
+      </View>
         <TouchableOpacity style={styles.button} onPress={handleLogin}>
           <Text style={styles.buttonText}>Giriş Yap</Text>
         </TouchableOpacity>
@@ -70,10 +92,20 @@ export default function LoginScreen({ navigation }) {
         <TouchableOpacity onPress={() => navigation.navigate('RegisterScreen')}>
           <Text style={styles.link}>Üye değil misin? Kayıt ol</Text>
         </TouchableOpacity>
+        {isLoading && (
+          <View style={styles.loadingOverlay}>
+            <View style={styles.loadingBox}>
+              <ActivityIndicator size="large" color="#6c63ff" />
+              <Text style={styles.loadingText}>Giriş yapılıyor...</Text>
+            </View>
+          </View>
+        )}
       </ScrollView>
     </KeyboardAvoidingView>
+    
   );
 }
+
 
 const styles = StyleSheet.create({
   keyboardContainer: {
@@ -121,4 +153,51 @@ const styles = StyleSheet.create({
     fontFamily: 'ms-regular',
     fontSize: 14,
   },
+  loadingOverlay: {
+  position: 'absolute',
+  top: 0,
+  left: 0,
+  right: 0,
+  bottom: 0,
+  backgroundColor: 'rgba(0,0,0,0.3)',
+  justifyContent: 'center',
+  alignItems: 'center',
+  zIndex: 10,
+},
+
+loadingBox: {
+  backgroundColor: '#fff',
+  paddingVertical: 20,
+  paddingHorizontal: 40,
+  borderRadius: 16,
+  elevation: 6,
+  shadowColor: '#000',
+  shadowOffset: { width: 0, height: 2 },
+  shadowOpacity: 0.3,
+  shadowRadius: 4,
+},
+
+loadingText: {
+  fontSize: 16,
+  fontFamily: 'ms-regular',
+  color: '#6c63ff',
+},
+passwordContainer: {
+  flexDirection: 'row',
+  alignItems: 'center',
+  borderWidth: 0.5,
+  borderColor: '#ddd',
+  backgroundColor: '#f9f9f9',
+  borderRadius: 18,
+  paddingHorizontal: 14,
+  marginBottom: 16,
+},
+
+passwordInput: {
+  flex: 1,
+  paddingVertical: 14,
+  fontSize: 16,
+  fontFamily: 'ms-regular',
+  color: '#333',
+},
 });
